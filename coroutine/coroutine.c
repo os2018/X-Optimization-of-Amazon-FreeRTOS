96c3c0e9e3067a448774567c2f80cced4c7d49af
mbinary <zhuheqin1@gmail.com>
2018-06-19 20:09:42 +0800

    添加到延迟队列

--- coroutine/coroutine.c
+++ coroutine/coroutine.c
@@ -2,50 +2,38 @@
 #include "tak.h"
 #include "croutine.h"
 #include "mylist.c"
-
 #define MAX_PRIORITY 100
 #if(configUSE_CO_ROUTINES !=0)
-
 #ifdef portREMOVE_STATIC_QUALIFIER 
     #define static 
 #endif 
 
 
 static list pReadyLists[MAX_PRIORITY]; // 协程队列, 处于 ready 状态, 有不同优先级
-static list delayedList ;       // 延时的协程队列
-static list overflowedList;   //   超时的
 static list pendingReadyList;   // 待 ready 的, 这些协程不能直接放到 ready 队列, 因为他们不能中断
-
-static list * pDelayed;
-static list *pOverflowed;
-
-
-CRCB_t * pCurrentCoroutine = NULL;
-
+static list  Delayed;  // 延时的协程队列
+static list Overflowed;  //   超时的
+static list* delayedList ;       
+static list* overflowedList;  
+CRCB_t * CUR = NULL;
 static UBaseType_t maxReadyPriority = 0;
 static TickType_t ctCur  = ctLast = ctPassed = 0;
-
 #define corINITIAL_STATE (0)  
 // 初始化状态
 //
-
-
-
 //协程的编写, 使用宏, 使代码易于读懂, 而且节省代码量.
 //这里作用是将一个协程加入就绪队列, 首先判断 它的优先级是否高于规定的最大优先级, 如果高于, 则设为最大优先级
 #define AddReadyCoroutine (pxCRCB)  { if(pxCRCB -> uxPriority  > maxReadyPriority){maxReadyPriority = pxCRCB -> uxPriority ;} listAppend(( list*)&(pReadyLists[pxCRCB->uxPriority ] ),&(pxCRCB ->xGenericListItem)) ;} 
-
 static void pListInit(viod);
 static void pCheckPendingList(void);
 static void pCheckDelayedList(void);
-
-BaseType_t createCoroutine( crCOROUTINE_CODE code, UBaseType_t priority, UBaseType_t  index)
+BaseType_t addReady( crCOROUTINE_CODE code, UBaseType_t priority, UBaseType_t  index)
 {
-    CRCB_t * pxCoRoutine;
-    pxCoRoutine  = (CRCB_t *) pvPortMalloc (sizeof(CRCB_t));
-    if(pxCoRoutine){
+    CRCB_t * NEW_COROUTINE;
+    NEW_COROUTINE  = (CRCB_t *) pvPortMalloc (sizeof(CRCB_t));
+    if(NEW_COROUTINE){
         if(pCurrentCoroutine == NULL){
-            pCurrentCoroutine - pxCoRoutine ;
+            pCurrentCoroutine - NEW_COROUTINE ;
             pListInit();
         }
         if(priority >= MAX_PRIORITY ){
@@ -53,25 +41,39 @@ BaseType_t createCoroutine( crCOROUTINE_CODE code, UBaseType_t priority, UBaseTy
             //此优先级作为数组的下标, 由于从0 开始, 所以 减去1
         }
         // 设置就绪队列协程的 一些基本数据 (通过传入的参数)
-		pxCoRoutine->uxState = corINITIAL_STATE; 
-		pxCoRoutine->uxPriority = priority ;
-		pxCoRoutine->uxIndex = index; 
-		pxCoRoutine->pxCoRoutineFunction = code;
+		NEW_COROUTINE->uxState = corINITIAL_STATE; 
+		NEW_COROUTINE->uxPriority = priority ;
+		NEW_COROUTINE->uxIndex = index; 
+		NEW_COROUTINE->NEW_COROUTINEFunction = code;
         
         
-        listInit( &(pxCoRoutine ->xGenericListItem ));
-        listInit( &(pxCoRoutine ->xEventListItem  ));
-
-        listPass(j &(pxCoRoutine ->xGenericListItem ),pxCoRoutine );
-        listPass( &(pxCoRoutine ->xEventListItem  ),pxCoRoutine );
-        
-		listSET_LIST_ITEM_VALUE( &( pxCoRoutine->xEventListItem ), ( ( TickType_t )MAX_PRIORITY - ( TickType_t ) priority  ) );
-
-
+        listInit( &(NEW_COROUTINE ->xGenericListItem ));
+        listInit( &(NEW_COROUTINE ->xEventListItem  ));
+        setOwner( &(NEW_COROUTINE ->xGenericListItem ),NEW_COROUTINE );
+        setOwner( &(NEW_COROUTINE ->xEventListItem  ),NEW_COROUTINE );
+       
+		setValue( &( NEW_COROUTINE->xEventListItem ), ( ( TickType_t )MAX_PRIORITY - ( TickType_t ) priority  ) );
         //所有准备工作完成, 可以加入就绪队列了(●ˇ∀ˇ●)
-        listAppend( pxCoRoutine );
-
+        listAppend( NEW_COROUTINE );
 		rerturn  pdPASS;
     }
 }
-/*-----------------------------------------------------------*/
+void addDelayed( TickType_t DelayedTick, list *events )
+{
+    TickType_t WakeTick;
+	WakeTick = xCoRoutineTickCount + DelayedTick;
+	( void ) listRemove( ( item * ) &( CUR->xGenericListItem ) );
+	/* 按唤醒时间排序插入 */
+	setValue( &( CUR->xGenericListItem ), WakeTick );
+	if( WakeTick < xCoRoutineTickCount ){
+		//这种情况发生溢出, 将其加入溢出队列
+		listAppend( ( list * ) overflowedList, ( item * ) &( CUR->xGenericListItem ) );
+	}else{
+		//否则加入延迟队列
+		listAppend( ( list * ) delayedList, ( item * ) &( CUR->xGenericListItem ) );
+	}
+	if( events ){
+		/* 如果有事件, 则加入事件队列*/
+		listAppend( events, &( CUR->xEventListItem ) );
+	}
+}
